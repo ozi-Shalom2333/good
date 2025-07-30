0f1f1fbad1eccd274d94c082625bd3b9c0d80c9f
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-07-30 18:07:37 +0100

    tackled responsivemess

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,33 @@
-import './navbar.css'
+import { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi"; 
+import "./navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false); 
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className='nav'>
-       <div className="nav-logo">EV-olution</div>
-       <ul className="nav-menu">
+    <div className="nav">
+      <div className="nav-logo">EV-olution</div>
+      <button
+        className="nav-toggle"
+        onClick={toggleMenu}
+        aria-label={isOpen ? "Close menu" : "Open menu"}
+        aria-expanded={isOpen}
+      >
+        {isOpen ? <HiX size={24} /> : <HiMenu size={24} />}
+      </button>
+      <ul className={`nav-menu ${isOpen ? "open" : ""}`}>
         <li>Home</li>
         <li>Explore</li>
         <li>About</li>
-        <li className='nav-contact'>Contact</li>
-       </ul>
+        <li className="nav-contact">Contact</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
